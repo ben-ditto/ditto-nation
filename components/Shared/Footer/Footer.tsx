c441f82cfa261fb385bb312419ab55a3aac31761
jasonandrew <jasonxandrewth@gmail.com>
2022-11-08 19:46:51 +0100

    Footer Astronauts

--- components/Shared/Footer/Footer.tsx
+++ components/Shared/Footer/Footer.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import socketIOClient from "socket.io-client";
 
 //Components
 import Marquee from "react-fast-marquee";
 import { Patreon, Instagram } from "components/Icons";
 
+const ENDPOINT = "http://127.0.0.1:4001";
+const TESTAPI = "http://api.open-notify.org/astros.json";
+
 const Footer = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await axios("http://api.open-notify.org/astros.json");
+
+        console.log(result.data.people);
+        let tempArr = result.data.people.map((obj) => {
+          return `${obj.name}: ${obj.craft}`;
+        });
+        setList(tempArr);
+      } catch {
+        console.log("eror");
+      }
+    };
+
+    getData();
+  }, []);
+
+  // useEffect(() => {
+  //   const socket = socketIOClient(ENDPOINT);
+
+  //   socket.on("FromAPI", (data) => {
+  //     let tempArr = data;
+  //     setList(tempArr);
+  //   });
+
+  //   //Cleanup
+  //   return () => {
+  //     socket.disconnect();
+  //   };
+  // }, []);
+
   return (
     <footer className="fixed bottom-0 left-0 z-50 w-screen bg-black text-white grid grid-cols-[auto_auto]">
       <div className="flex border-r border-r-white p-4">
@@ -28,13 +67,11 @@ const Footer = () => {
       <Marquee
         pauseOnHover
         gradient={false}
-        className="p-4 uppercase font-bold"
+        className="p-4 uppercase font-bold !min-w-[70%]"
       >
-        DOOM SCROLL - Doom scroll - Doom scroll - Doom scroll - Doom scroll -
-        Doom scroll - DOOM SCROLL - Doom scroll - Doom scroll - Doom scroll -
-        Doom scroll - Doom scroll - DOOM SCROLL - Doom scroll - Doom scroll -
-        Doom scroll - Doom scroll - Doom scroll - DOOM SCROLL - Doom scroll -
-        Doom scroll - Doom scroll - Doom scroll - Doom scroll -
+        {list.map((el, idx) => (
+          <span key={idx}>{el + " - "}</span>
+        ))}
       </Marquee>
     </footer>
   );
