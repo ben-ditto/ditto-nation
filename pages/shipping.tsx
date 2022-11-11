3be61868de7168bf4c21d0346b94d1ad323f878e
jasonandrew <jasonxandrewth@gmail.com>
2022-11-11 13:50:10 +0100

    Shipping page layout

--- pages/shipping.tsx
+++ pages/shipping.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { NextSeo } from "next-seo";
 
 import { shopifyGraphqlRequestClient } from "src/lib/clients/graphqlRequestClient";
 
 import { dehydrate, QueryClient } from "@tanstack/react-query";
 
-import Layout from "components/Layout";
 import Loader from "components/UI/Loader";
+
+//Layout
+import { getLayout } from "components/Layout/Layout";
 
 import { useGetShopInfoQuery, GetShopInfoQuery } from "src/generated/graphql";
 
@@ -19,16 +22,17 @@ const ShippingPage = () => {
   if (error) return null;
 
   return (
-    <Layout
-      main={
-        <div
-          className="max-w-8xl"
-          dangerouslySetInnerHTML={{ __html: data?.shop?.shippingPolicy?.body }}
-        />
-      }
-    />
+    <>
+      <NextSeo title="Shipping" />
+      <div
+        className="max-w-8xl"
+        dangerouslySetInnerHTML={{ __html: data?.shop?.shippingPolicy?.body }}
+      />
+    </>
   );
 };
+
+ShippingPage.getLayout = getLayout;
 
 export default ShippingPage;
 
