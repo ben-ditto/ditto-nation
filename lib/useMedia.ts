48189f180b4a76cc0eb772bfaed6f1b9fc27aa90
jasonandrew <jasonxandrewth@gmail.com>
2022-12-06 17:01:32 +0100

    media hook product view

--- lib/useMedia.ts
+++ lib/useMedia.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const useMedia = () => {
   const [isDesktop, setIsDesktop] = useState(false);
   useEffect(() => {
-    const media = window.matchMedia("(min-width: 960px)");
+    const media = window.matchMedia("(min-width: 660px)");
     const listener = () => setIsDesktop(media.matches);
     listener();
     window.addEventListener("resize", listener);
@@ -14,3 +14,44 @@ const useMedia = () => {
 };
 
 export default useMedia;
+
+export function useMediaQuery(query: string): boolean {
+  const getMatches = (query: string): boolean => {
+    // Prevents SSR issues
+    if (typeof window !== "undefined") {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  };
+
+  const [matches, setMatches] = useState<boolean>(getMatches(query));
+
+  function handleChange() {
+    setMatches(getMatches(query));
+  }
+
+  useEffect(() => {
+    const matchMedia = window.matchMedia(query);
+
+    // Triggered at the first client-side load and if query changes
+    handleChange();
+
+    // Listen matchMedia
+    if (matchMedia.addListener) {
+      matchMedia.addListener(handleChange);
+    } else {
+      matchMedia.addEventListener("change", handleChange);
+    }
+
+    return () => {
+      if (matchMedia.removeListener) {
+        matchMedia.removeListener(handleChange);
+      } else {
+        matchMedia.removeEventListener("change", handleChange);
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  return matches;
+}
